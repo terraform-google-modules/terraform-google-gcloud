cc40d77a8c2286110d75b0cd1f6c6b6f988120e4
Miles Matthias <mmatthias@google.com>
2019-10-29 10:50:13 -0600

    remove old outputs & regenerate docs

--- examples/simple_example/outputs.tf
+++ examples/simple_example/outputs.tf
@@ -13,28 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-output "gcloud" {
-  description = "Path to gcloud CLI"
-  value       = module.cli.gcloud
-}
-
-output "bq" {
-  description = "Path to bq CLI"
-  value       = module.cli.bq
-}
-
-output "gsutil" {
-  description = "Path to gsutil CLI"
-  value       = module.cli.gsutil
-}
-
-output "kubectl" {
-  description = "Path to kubectl CLI. Must be installed first using additional_components"
-  value       = module.cli.kubectl
-}
-
-output "gcloud_bin_path" {
-  description = "Path to gcloud bin path for use to locate any other components"
-  value       = module.cli.gcloud_bin_path
-}
