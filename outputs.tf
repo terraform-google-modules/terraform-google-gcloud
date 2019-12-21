a1d20133ef4b0c5fbf4886afee7d9c1f88ad4a23
Taylor Ludwig <taylorludwig@gmail.com>
2019-12-21 14:09:45 -0800

    add back in vars that were used for outputs
    Former-commit-id: b0bdd8c777816983b259a904e5a95e2bcc5c6318

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,8 @@ output "destroy_cmd_bin" {
   description = "The full bin path & command used on destroy"
   value       = local.destroy_cmd_bin
 }
+
+output "bin_dir" {
+  description = "The full bin path of the modules executables"
+  value       = local.gcloud_bin_path
+}
