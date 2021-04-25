12c820ca1fa3ae37e819c106d99bdb25d52c9b68
Yash Bhutwala <ymb002@bucknell.edu>
2021-04-25 08:54:28 -0400

    make it backwards compatible

--- modules/kubectl-wrapper/main.tf
+++ modules/kubectl-wrapper/main.tf
@@ -15,7 +15,7 @@
  */
 
 locals {
-  base_cmd = "${var.cluster_name} ${var.cluster_location} ${var.project_id} ${var.internal_ip} ${var.use_existing_context} ${var.impersonate_service_account}"
+  base_cmd = "${var.cluster_name} ${var.cluster_location} ${var.project_id} ${var.internal_ip} ${var.use_existing_context}"
 }
 
 module "gcloud_kubectl" {
@@ -29,8 +29,8 @@ module "gcloud_kubectl" {
   service_account_key_file = var.service_account_key_file
 
   create_cmd_entrypoint  = "${path.module}/scripts/kubectl_wrapper.sh"
-  create_cmd_body        = "${local.base_cmd} ${var.kubectl_create_command}"
+  create_cmd_body        = var.impersonate_service_account ? "${local.base_cmd} ${var.impersonate_service_account} ${var.kubectl_create_command}" : "${local.base_cmd} ${var.kubectl_create_command}"
   create_cmd_triggers    = var.create_cmd_triggers
   destroy_cmd_entrypoint = "${path.module}/scripts/kubectl_wrapper.sh"
-  destroy_cmd_body       = "${local.base_cmd} ${var.kubectl_destroy_command}"
+  destroy_cmd_body       = var.impersonate_service_account ? "${local.base_cmd} ${var.impersonate_service_account} ${var.kubectl_destroy_command}" : "${local.base_cmd} ${var.kubectl_destroy_command}"
 }
