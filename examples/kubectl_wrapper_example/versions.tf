85ddf67159c40ec7c385b62e284cea8f09d7d35f
bharathkkb <bharathkrishnakb@gmail.com>
2021-11-22 13:02:42 -0600

    pin example using gke to 3.x

--- examples/kubectl_wrapper_example/versions.tf
+++ examples/kubectl_wrapper_example/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 3.53"
     }
   }
   required_version = ">= 0.13"
