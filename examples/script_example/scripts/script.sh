2a036802f63e9b6d41b72d4d5950df1da52279ec
Zaar Hai <haizaar@users.noreply.github.com>
2022-03-10 04:53:29 +1100

    fix: Switch to using bash env to make module work on NixOS and BSD (#117)
    * POSIX compliance - to make it work on NixOS and BSD
    
    * switching to bash to make shellcheck happy

--- examples/script_example/scripts/script.sh
+++ examples/script_example/scripts/script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh
 
 GCLOUD_LOCATION=$(command -v gcloud)
 echo "Using gcloud from $GCLOUD_LOCATION"
