952413a1031bb7fb1de39a5980b527ef0771bbc7
Andrew Peabody <apeabody@gmail.com>
2022-06-22 10:46:59 -0700

    Update examples/kubectl_wrapper_example/main.tf
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- examples/kubectl_wrapper_example/main.tf
+++ examples/kubectl_wrapper_example/main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "enabled_google_apis" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 13"
+  version = "~> 13.0"
 
   project_id                  = var.project_id
   disable_services_on_destroy = false
